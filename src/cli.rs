8006d69aa701cb50723e17efd0481ab455c219f1
Christian Mösl <christian.moesl@live.at>
2022-04-23 22:19:59 +0200

    add unit tests for CLI argument parser

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,6 @@
+use anyhow::{Context, Result};
 use clap::Parser;
+use std::ffi::OsString;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -10,6 +12,42 @@ pub struct Args {
     pub command: Vec<String>,
 }
 
-pub fn parse_arguments() -> Args {
-    Args::parse()
+pub fn parse_arguments(args: &[OsString]) -> Result<Args> {
+    Args::try_parse_from(args).context("failed to parse arguments")
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::str::FromStr;
+
+    fn os(string: &str) -> OsString {
+        OsString::from_str(string).unwrap()
+    }
+
+    #[test]
+    fn test_minimum_required_arguments() {
+        let result = parse_arguments(&[os("benchie")]);
+
+        assert!(result.is_err(), "should has at least one argument");
+    }
+
+    #[test]
+    fn test_benchmark_command() {
+        let result = parse_arguments(&[os("benchie"), os("time"), os("--show")]);
+
+        assert!(result.is_ok(), "should succeed to parse benchmark command");
+
+        let args = result.unwrap();
+
+        assert_eq!(args.command.len(), 2, "command should has length 2");
+        assert_eq!(
+            args.command[0], "time",
+            "first part of command should be time"
+        );
+        assert_eq!(
+            args.command[1], "--show",
+            "second part of command should be --show"
+        );
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use anyhow::Result;
 use benchie::benchmark;
+use std::env;
 
 mod cli;
 
 fn main() -> Result<()> {
-    let args = cli::parse_arguments();
+    let raw_args: Vec<_> = env::args_os().collect();
+
+    let args = cli::parse_arguments(&raw_args)?;
 
     benchmark(&args.command)
 }
