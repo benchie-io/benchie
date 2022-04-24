6fe87de25e781177282749be7fc1a09b8e247bde
Christian Mösl <christian.moesl@live.at>
2022-04-24 21:13:29 +0200

    add crash reporter with Github link generator

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 mod benchmark;
+mod crash_report;
 mod show;
 mod storage;
 
 pub use benchmark::benchmark;
+pub use crash_report::initialize_crash_reporter;
 pub use show::show;
 pub use storage::{append_benchmark, load_all_benchmarks, Benchmark};
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use anyhow::Result;
-use benchie::benchmark;
 use benchie::show;
+use benchie::{benchmark, initialize_crash_reporter};
 use std::env;
 
 mod cli;
 
 fn main() -> Result<()> {
+    initialize_crash_reporter();
+
     let raw_args: Vec<_> = env::args_os().collect();
     let args = cli::parse_arguments(&raw_args)?;
 
